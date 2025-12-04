539463cf1afc72e26df1b699d66eac5be5fe557e
maria <maria@example.com>
2025-12-04 11:47:26 +0200

    Push full MVC Calculator project

---
+++ Controllers/CalculatorController.cs
@@ -0,0 +1,36 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+using projectqa.Models;
+
+
+
+   namespace projectqa.Controllers
+{
+        public class CalculatorController : Controller
+        {
+            private readonly Calculator _calculator = new Calculator();
+
+            public IActionResult Index()
+            {
+                return View();
+            }
+
+            [HttpPost]
+            public IActionResult Calculate(double a, double b, string operation)
+            {
+                double result = operation switch
+                {
+                    "Add" => _calculator.Add(a, b),
+                    "Subtract" => _calculator.Subtract(a, b),
+                    "Multiply" => _calculator.Multiply(a, b),
+                    "Divide" => _calculator.Divide(a, b),
+                    _ => 0
+                };
+
+                ViewBag.Result = result;
+                return View("Index");
+            }
+        }
+    }
+
+
---
+++ Views/Calculator/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace projectqa.Views.Calculator
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
